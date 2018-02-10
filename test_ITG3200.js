d43965ea3e9114e64435a9fa576975c192f4cc7e
luispuertolas <lapb@protonmail.com>
2018-02-10 02:24:58 +0000

    Add files via upload

--- test_ITG3200.js
+++ test_ITG3200.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 I2C1.setup( { scl: B6, sda: B7 } );
 var gyro = require('https://github.com/polhomarkho/espruino-itg-3200/blob/master/ITG3200.js').connect( I2C1 );
 
@@ -15,3 +16,22 @@ setInterval( function () {
   console.log("z : " + d.z);
   console.log(" ");
 }, 1000);
+=======
+I2C1.setup( { scl: B6, sda: B7 } );
+var gyro = require('https://github.com/polhomarkho/espruino-itg-3200/blob/master/ITG3200.js').connect( I2C1 );
+
+console.log("Calibrating offset...");
+
+gyro.calibrateOffset();
+
+console.log("Getting values...");
+
+setInterval( function () {
+  d = gyro.read();
+  console.log("temp : " + d.temp);
+  console.log("x : " + d.x);
+  console.log("y : " + d.y);
+  console.log("z : " + d.z);
+  console.log(" ");
+}, 1000);
+>>>>>>> 991ab91f3a25fc17651860e8c8c0a71c72497ce7
